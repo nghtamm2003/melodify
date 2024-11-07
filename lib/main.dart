a60b1c394e23404dda05c35475419f7ac2ef1acc
CeleX <tachibanakanade2003@gmail.com>
2024-11-07 20:40:59 +0700

    feat: initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:melodify/core/theme/app_theme.dart';
 import 'package:melodify/features/get_started/bloc/theme_cubit.dart';
 import 'package:melodify/features/get_started/views/pages/get_started.dart';
+import 'package:melodify/firebase_options.dart';
 import 'package:path_provider/path_provider.dart';
 
 Future<void> main() async {
@@ -13,6 +16,12 @@ Future<void> main() async {
     storageDirectory: kIsWeb
         ? HydratedStorage.webStorageDirectory
         : await getApplicationDocumentsDirectory(),
+  );
+  await dotenv.load(
+    fileName: '.env',
+  );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
